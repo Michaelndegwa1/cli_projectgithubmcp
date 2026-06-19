e8da196ba259352f1557f39556994b188bb19f13
Michaelndegwa1 <michaelndegwa997@gmail.com>
2026-06-19 20:56:00 +0300

    Implement read_resource method to fetch and parse resource contents

--- mcp_client.py
+++ mcp_client.py
@@ -1,5 +1,6 @@
 import sys
 import asyncio
+from pydantic import AnyUrl
 from typing import Optional, Any
 from contextlib import AsyncExitStack
 from mcp import ClientSession, StdioServerParameters, types
@@ -58,8 +59,14 @@ class MCPClient:
         return []
 
     async def read_resource(self, uri: str) -> Any:
-        # TODO: Read a resource, parse the contents and return it
-        return []
+       result=await self.session().read_resource(uri)
+       resource=result.contents[0]
+       
+       if isinstance(resource, types.TextResourceContents):
+        if resource.mimeType == "application/json":
+            return json.loads(resource.text)
+    
+        return resource.text
 
     async def cleanup(self):
         await self._exit_stack.aclose()
